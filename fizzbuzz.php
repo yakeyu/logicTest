bf9158b2043ff7dbf20766a04c89c76811cbe7e0
YujiYakena <tengan28@gmail.com>
2019-03-23 18:14:30 +0900

    first commit

---
+++ fizzbuzz.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>FizzBuzz</title>
+</head>
+<body>
+
+  <?php 
+
+  echo "hello world";
+
+
+
+
+ ?>
+  
+</body>
+</html>
