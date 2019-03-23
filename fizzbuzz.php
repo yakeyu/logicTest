159420be653d1e19359a1874c5493c1332df267c
YujiYakena <tengan28@gmail.com>
2019-03-23 18:31:36 +0900

    ５の倍数のときに"Buzz"を出力

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -12,8 +12,11 @@
       if($i % 3 == 0 && $i % 5 == 0) {
         echo "FizzBuzz";
         echo "<br>";
-      } elseif($i % 3 == 0){
+      } elseif($i % 3 == 0) {
         echo "Fizz";
+        echo "<br>";
+      } elseif($i % 5 == 0) {
+        echo "Buzz";
         echo "<br>";
       } else {
         echo $i;
