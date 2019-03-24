d592ebf580141ca39e3122f8597d3786c8962113
YujiYakena <tengan28@gmail.com>
2019-03-25 01:59:58 +0900

    コメントの挿入

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -8,17 +8,25 @@
 
   <?php 
 
+    // $iが100に達するまでループ
     for($i = 1; $i <= 100; $i++) {
+      // もし$iが３の倍数かつ５の倍数の時
       if($i % 3 == 0 && $i % 5 == 0) {
+        // "FizzBuzz"を出力
         echo "FizzBuzz";
         echo "<br>";
+        // もし$iが３の倍数の時
       } elseif($i % 3 == 0) {
+        // "Fizz"を出力
         echo "Fizz";
         echo "<br>";
+        // もし$iが5の倍数の時
       } elseif($i % 5 == 0) {
+        // "Buzz"を出力
         echo "Buzz";
         echo "<br>";
       } else {
+        // ３の倍数でも５の倍数でもない時、$iを出力
         echo $i;
         echo "<br>";
       }
